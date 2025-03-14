df1d0f7f02076054bdf6ea1163a430b950a3b9eb
Дмитрий <dichernyshov@edu.hse.ru>
2025-03-14 12:40:32 +0300

    Update storage.go

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,10 +5,10 @@ import "time"
 const storageTime = 24 * 7 * time.Hour
 
 type Storage interface {
-	// ShortenAndSaveUrl сохраняет оригинальный shortUrl и возвращает укороченный
+	// ShortenAndSaveUrl сохраняет оригинальный Url и возвращает укороченный
 	ShortenAndSaveUrl(url string) (string, error)
 
-	// GetUrl возвращает оригинальный shortUrl по укороченному
+	// GetUrl возвращает оригинальный Url по укороченному
 	GetUrl(shortUrl string) (string, error)
 
 	// Close завершает работу хранилища
